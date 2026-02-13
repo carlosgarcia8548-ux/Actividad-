8ddde7e735f3a4ec98799e5654da60d0e880156a
carlosgarcia8548-ux <carlos.garcia8548@alumnos.udg.mx>
2026-02-13 13:48:21 -0600

    Add files via upload

---
+++ Par.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+int main()
+{
+    int a;
+    printf("Ingresa un numero");
+    scanf("%d", &a);
+
+    if(a % 2 == 0)
+        printf("Es par");
+    else
+        printf("No es par");
+
+    return 0;
+}*/
+
